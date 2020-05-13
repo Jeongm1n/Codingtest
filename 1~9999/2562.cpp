b48f47e1003ab3817fc200a2ee83f7d49a3b1a16
all4359 <qll4359@naver.com>
2020-05-13 21:50:41 +0900

    code

---
+++ 1~9999/2562.cpp
@@ -0,0 +1,17 @@
+/*#include<iostream>
+using namespace std;
+int main() {
+	int arr[9], max, index=0;
+	for (int i = 0; i < 9; i++)
+		cin >> arr[i];
+	max = arr[0];
+	for (int i = 0; i < 9; i++) {
+		if (max < arr[i]) {
+			max = arr[i];
+			index = i;
+		}
+	}
+	cout << max << endl;
+	cout << index+1 << endl;
+	return 0;
+}*/
