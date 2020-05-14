1314ba09911ca943857c2ba542667d35ccc0af05
all4359 <qll4359@naver.com>
2020-05-14 21:23:51 +0900

    code

---
+++ 1~9999/2675.cpp
@@ -0,0 +1,17 @@
+/*#include<iostream>
+using namespace std;
+int main() {
+	int n, num; char str[20];
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		cin >> str;
+		for (int j = 0; str[j]; j++) {
+			for (int k = 0; k<num; k++) {
+				cout << str[j];
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}*/
