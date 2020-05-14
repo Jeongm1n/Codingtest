ab6982382f322049dfdaa0da07ad7248aa2f8aef
all4359 <qll4359@naver.com>
2020-05-14 15:56:43 +0900

    code

---
+++ 1~9999/2588.cpp
@@ -0,0 +1,17 @@
+/*#include<iostream>
+using namespace std;
+int main() {
+	char arr1[3];
+	char arr2[3];
+	int sum1 = 0, sum2 = 0, sum3 = 0;
+	cin >> arr1;
+	cin >> arr2;
+	sum1 = ((arr2[2] - 48) * (arr1[2] - 48)) + (((arr2[2] - 48) * (arr1[1] - 48)) * 10) + (((arr2[2] - 48) * (arr1[0] - 48)) * 100);
+	sum2= ((arr2[1] - 48) * (arr1[2] - 48)) + (((arr2[1] - 48) * (arr1[1] - 48)) * 10) + (((arr2[1] - 48) * (arr1[0] - 48)) * 100);
+	sum3 = ((arr2[0] - 48) * (arr1[2] - 48)) + (((arr2[0] - 48) * (arr1[1] - 48)) * 10) + (((arr2[0] - 48) * (arr1[0] - 48)) * 100);
+	cout << sum1 << endl;
+	cout << sum2 << endl;
+	cout << sum3 << endl;
+	cout << sum1 + (sum2*10) + (sum3*100) << endl;
+	return 0;
+}*/
