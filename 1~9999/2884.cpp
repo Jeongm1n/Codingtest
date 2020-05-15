25911e641024daaebd3c8cc5e55637b5799ff6f8
all4359 <qll4359@naver.com>
2020-05-15 18:49:16 +0900

    code

---
+++ 1~9999/2884.cpp
@@ -0,0 +1,11 @@
+/*#include<iostream>
+using namespace std;
+int main() {
+	int h, m, sum = 0;
+	cin >> h >> m;
+	if (h == 0&&m<45)
+		h = 24;
+	sum = ((h * 60) + m)-45;
+	cout << sum / 60 << " " << sum % 60 << endl;
+	return 0;
+}*/
