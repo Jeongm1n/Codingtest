06a5b2db1c6c94f1eaf172a20d93f6ac0b3f1fc8
all4359 <qll4359@naver.com>
2020-05-16 16:42:01 +0900

    code

---
+++ 10000~/11720.cpp
@@ -0,0 +1,16 @@
+/*#include<iostream>
+using namespace std;
+int main() {
+	char str[100];
+	int n;
+	int result = 0;
+	int sum = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> str[i];
+		result = str[i] - 48;
+		sum += result;
+	}
+	cout << sum << endl;
+	return 0;
+}*/
