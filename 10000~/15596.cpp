89dd55edeb952643b87f75f1c1fc3615781af531
all4359 <qll4359@naver.com>
2020-05-17 19:45:55 +0900

    code

---
+++ 10000~/15596.cpp
@@ -0,0 +1,20 @@
+/*#include<iostream>
+#include<vector>
+using namespace std;
+long sum(std::vector<int>& a) {
+	int size = a.size();
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+		sum += a[i];
+	return sum;
+}
+int main() {
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	cout << sum(arr) << endl;
+	return 0;
+}*/
