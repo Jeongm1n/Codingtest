cec21519e16f8e3adc65f12e0e8a6df7f2f29fe8
all4359 <qll4359@naver.com>
2020-05-18 16:28:40 +0900

    code

---
+++ 10000~/11721.cpp
@@ -0,0 +1,14 @@
+/*#include<iostream>
+using namespace std;
+int main() {
+	char str[100];
+	int i = 0;
+	cin >> str;
+	while (str[i]) {
+		cout << str[i];
+		if ((i+1) % 10 == 0)
+			cout << endl;
+		i++;
+	}
+	return 0;
+}*/
