225d1f3ed35967949c0017b966f8891992dea6fc
all4359 <qll4359@naver.com>
2020-05-27 16:10:03 +0900

    code

---
+++ 10000~/10817.cpp
@@ -0,0 +1,18 @@
+/*#include<iostream>
+using namespace std;
+int main() {
+	int arr[3], temp=0;
+	for (int i = 0; i < 3; i++)
+		cin >> arr[i];
+	for (int i = 0; i < 3; i++) {
+		for (int j = i; j < 3; j++) {
+			if (arr[i] > arr[j]) {
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	cout << arr[1] << endl;
+	return 0;
+}*/
