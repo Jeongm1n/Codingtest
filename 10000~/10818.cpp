bfcabd8402ad3032d2b7b170506d521a1faf0e1f
all4359 <qll4359@naver.com>
2020-05-28 16:34:12 +0900

    code

---
+++ 10000~/10818.cpp
@@ -0,0 +1,20 @@
+/*#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	int* arr;
+	cin >> n;
+	arr = new int[n];
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	int min = arr[0];
+	int max = arr[0];
+	for (int i = 0; i < n; i++) {
+		if (max < arr[i])
+			max = arr[i];
+		if (min > arr[i])
+			min = arr[i];
+	}
+	cout << min << " " << max << endl;
+	return 0;
+}*/
