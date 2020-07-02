c415f71784d3b40f1f675e1f2e75346f243e27a1
all4359 <qll4359@naver.com>
2020-07-02 21:24:37 +0900

    Skill Check

---
+++ Lv.1/Problem.1.py
@@ -0,0 +1,21 @@
+import math
+def solution(n):
+    answer = 0
+    length = str(n)
+    arr = []
+    size_ = len(length)-1
+    for i in range(len(length)):
+        size=math.pow(10,size_)
+        arr.append(int(n/size))
+        n=n-(arr[i]*size)
+        size_ -= 1
+    arr.sort(reverse=True)
+    size_ = len(length)-1
+    for i in range(len(length)):
+        size=math.pow(10,size_)
+        answer+=arr[i]*size
+        size_-=1
+    return answer
+
+num = int(input())
+print(int(solution(num)))
