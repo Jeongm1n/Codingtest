264485e24c5ce960425749cb879ace7aa493bc70
Jeongm1n <qll4359@naver.com>
2020-09-13 18:21:33 +0900

    Skill check

---
+++ Lv.2/Target_number.py
@@ -0,0 +1,29 @@
+def solution(numbers, target):
+    '''
+    cnt = 0
+    len_numbers = len(numbers)
+
+    def operator(idx=0):
+        if idx < len_numbers:
+            numbers[idx] *= 1
+            operator(idx+1)
+
+            numbers[idx] *= -1
+            operator(idx+1)
+        elif sum(numbers) == target:
+            nonlocal cnt
+            cnt += 1
+
+
+    operator()
+
+    return cnt
+    '''
+    from itertools import product   
+    i = [(x, -x) for x in numbers]
+    j = list(map(sum, product(*i)))
+    return j.count(target)
+    
+numbers=[1,1,1,1,1]
+target=3
+print(solution(numbers, target))
