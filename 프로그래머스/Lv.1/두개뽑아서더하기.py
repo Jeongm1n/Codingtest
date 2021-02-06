bfeda29bee907f59c267b2fcb6d0e8c1fca057fe
Jeongm1n <qll4359@naver.com>
2021-02-06 18:31:04 +0900

    Problem Solving

--- 프로그래머스/Lv.1/두개뽑아서더하기.py
+++ 프로그래머스/Lv.1/두개뽑아서더하기.py
@@ -1,12 +1,8 @@
 def solution(numbers):
     answer = []
-    for i in range(len(numbers)-1):
+    for i in range(len(numbers)):
         for j in range(i+1, len(numbers)):
             answer.append(numbers[i]+numbers[j])
-    
-    return sorted(list(set(answer)))
-
-numbers = [1,2,3,4,5]
+    return sorted(set(answer))
+numbers = list(map(int, input().split()))
 print(solution(numbers))
-  
-  
