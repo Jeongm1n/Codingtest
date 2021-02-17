405c7bf6429114185bf732c7c5c522ea2263645f
Jeongm1n <qll4359@naver.com>
2021-02-17 14:34:35 +0900

    Problem Solving

--- 프로그래머스/Lv.2/가장큰수.py
+++ 프로그래머스/Lv.2/가장큰수.py
@@ -1,8 +1,7 @@
 def solution(numbers):
     numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x:x*3, reverse=True)
+    numbers.sort(key=lambda x : x*3, reverse=True)
     print(numbers)
     return str(int(''.join(numbers)))
-
-numbers=[3,30,34,5,9]
+numbers = list(map(int, input().split()))
 print(solution(numbers))
