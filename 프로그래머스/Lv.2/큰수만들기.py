4cc20fdaef8cb81ed2426038bff463835f56bb4e
Jeongm1n <qll4359@naver.com>
2021-02-20 17:33:35 +0900

    Problem Solving

--- 프로그래머스/Lv.2/큰수만들기.py
+++ 프로그래머스/Lv.2/큰수만들기.py
@@ -1,21 +1,15 @@
 def solution(number, k):
-    collected = []  # 숫자를 모아 큰 수를 만들 때 쓰는 배열
-    
-    for i, num in enumerate(number): # enumerate는 인덱스 확인
-        # k개만큼의 숫자를 빼냈을 때, i의 인덱스를 기억하기 위해 i 사용
-        while len(collected) > 0 and collected[-1] < num and k > 0:
-            # 
+    collected = []
+    for (i, num) in enumerate(number):
+        while collected and collected[-1] < num and k > 0:
             collected.pop()
-            k-=1
-        if k==0:
-            collected += list(number[i:])
+            k -= 1
+        if k == 0:
+            collected += number[i:]
             break
         collected.append(num)
     collected = collected[:-k] if k > 0 else collected
-    
-    answer = ''.join(collected)
-    return answer
-
-number = "4177252841"
-k = 4
+    return ''.join(collected)
+number = input()
+k = int(input())
 print(solution(number, k))
