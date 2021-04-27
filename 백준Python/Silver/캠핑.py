787474329dca2cb3362f63d83e794ed3a2c17f05
Jeongm1n <qll4359@naver.com>
2021-04-27 20:22:23 +0900

    Problem Solving

---
+++ 백준Python/Silver/캠핑.py
@@ -0,0 +1,8 @@
+i = 1
+while 1:
+    l, p, v = map(int, input().split())
+    if l == 0 and p == 0 and v == 0:
+        break
+    answer = ((v//p)*l)+min((v%p), l)
+    print('Case %d: %d' %(i, answer))
+    i += 1
