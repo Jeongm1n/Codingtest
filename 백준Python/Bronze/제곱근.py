2079dd9dff490e1efae491664885d5959cdeaadd
Jeongm1n <qll4359@naver.com>
2021-05-02 16:57:27 +0900

    Problem Solving

---
+++ 백준Python/Bronze/제곱근.py
@@ -0,0 +1,12 @@
+n = int(input())
+start = 1
+end = n
+while start <= end:
+    mid = (start+end)//2
+    if mid**2 == n:
+        print(mid)
+        break
+    elif mid**2 > n:
+        end = mid - 1
+    else:
+        start = mid + 1
