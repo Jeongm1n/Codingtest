cd1e0698402920fe2668f89165c8ea24d891db55
Jeongm1n <qll4359@naver.com>
2021-05-02 16:58:14 +0900

    Problem Solving

--- 백준Python/Silver/수찾기.py
+++ 백준Python/Silver/수찾기.py
@@ -1,7 +1,31 @@
-import sys
-n = int(sys.stdin.readline())
-a = set(map(int, sys.stdin.readline().split()))
-m = int(sys.stdin.readline())
-b = list(map(int, sys.stdin.readline().split()))
-for num in b:
-    sys.stdout.write('1\n') if num in a else sys.stdout.write('0\n')
+# import sys
+# n = int(sys.stdin.readline())
+# a = set(map(int, sys.stdin.readline().split()))
+# m = int(sys.stdin.readline())
+# b = list(map(int, sys.stdin.readline().split()))
+# for num in b:
+#     sys.stdout.write('1\n') if num in a else sys.stdout.write('0\n')
+n = int(input())
+array = list(map(int, input().split()))
+array.sort()
+m = int(input())
+target = list(map(int, input().split()))
+
+def binary_search(array, t, start, end):
+    while start <= end:
+        mid = (start+end)//2
+        if array[mid] == t:
+            return mid
+        elif array[mid] > t:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+answer = 0
+for t in target:
+    answer = binary_search(array, t, 0, n-1)
+    if answer == None:
+        print(0)
+    else:
+        print(1)
