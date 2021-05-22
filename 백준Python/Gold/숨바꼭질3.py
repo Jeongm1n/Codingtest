c19043a56e2a2edbaf0cc1f59d3efdd386a38c18
Jeongm1n <qll4359@naver.com>
2021-05-22 20:11:33 +0900

    Problem Solving

---
+++ 백준Python/Gold/숨바꼭질3.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+n, k = map(int, input().split())
+graph = [0] * 100001
+
+dx = [-1, 1, 2]
+
+
+def bfs(start):
+    queue = deque()
+    queue.append(start)
+    while queue:
+        x = queue.popleft()
+        if x == k:
+            print(graph[x])
+            break
+        for i in range(3):
+            if i == 2:
+                nx = x * dx[i]
+            else:
+                nx = x + dx[i]
+            if 0 <= nx < 100001 and graph[nx] == 0:
+                if nx == x * 2 and x != 0:
+                    graph[nx] = graph[x]
+                    queue.appendleft(nx)
+                else:
+                    graph[nx] = graph[x] + 1
+                    queue.append(nx)
+
+
+bfs(n)
