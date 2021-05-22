288bdd28f6c5bf4c77611a3931c0b6ff34aed808
Jeongm1n <qll4359@naver.com>
2021-05-22 20:11:58 +0900

    Problem Solving

---
+++ 백준Python/Silver/RGB거리.py
@@ -0,0 +1,15 @@
+n = int(input())
+data = []
+for _ in range(n):
+    data.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    for j in range(3):
+        if j == 0:
+            data[i][j] = min(data[i - 1][1], data[i - 1][2]) + data[i][j]
+        elif j == 1:
+            data[i][j] = min(data[i - 1][0], data[i - 1][2]) + data[i][j]
+        elif j == 2:
+            data[i][j] = min(data[i - 1][0], data[i - 1][1]) + data[i][j]
+
+print(min(data[-1]))
