9f04fd4a1b6e5de60bdcfeee0bce89550bbeca22
Jeongm1n <qll4359@naver.com>
2021-05-25 01:22:24 +0900

    Problem Solving

---
+++ 백준Python/Silver/상근이의여행.py
@@ -0,0 +1,7 @@
+import sys
+
+for _ in range(int(sys.stdin.readline())):
+    n, m = map(int, sys.stdin.readline().split())
+    for _ in range(m):
+        a, b = map(int, sys.stdin.readline().split())
+    print(n - 1)
