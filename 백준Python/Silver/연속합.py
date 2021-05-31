f9b9a74170190092ee4db511de4831010b2266cd
Jeongm1n <qll4359@naver.com>
2021-05-31 22:50:52 +0900

    Problem Solving

---
+++ 백준Python/Silver/연속합.py
@@ -0,0 +1,8 @@
+n = int(input())
+arr = list(map(int, input().split()))
+d = [0] * n
+d[0] = arr[0]
+for i in range(1, n):
+    d[i] = max(arr[i - 1] + arr[i], arr[i], d[i - 1] + arr[i])
+
+print(max(d))
