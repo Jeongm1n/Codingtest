a1d727f7f50c68d9b11cedf7efa749a25bf05a34
Jeongm1n <qll4359@naver.com>
2021-06-01 18:00:33 +0900

    Problem Solving

--- 프로그래머스/Lv.1/내적.py
+++ 프로그래머스/Lv.1/내적.py
@@ -1,8 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+
 def solution(a, b):
     answer = 0
     for i in range(len(a)):
-        answer = answer + (a[i]*b[i])
+        answer += a[i] * b[i]
     return answer
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
+
+
 print(solution(a, b))
