df327c076f1724d304c2cd80a795739a2056ddb3
Jeongm1n <qll4359@naver.com>
2021-06-01 16:34:16 +0900

    Problem Solving

--- 프로그래머스/Lv.1/체육복.py
+++ 프로그래머스/Lv.1/체육복.py
@@ -1,14 +1,20 @@
-def solution(n, lost, reserve):
-    # 여벌 체육복에 관한 경우 적용
-    set_lost = set(lost) - set(reserve)
-    set_reserve = set(reserve) - set(lost)
-    for i in set_reserve:
-        if i-1 in set_lost:
-            set_lost.remove(i-1)
-        elif i+1 in set_lost:
-            set_lost.remove(i+1)
-    return n-len(set_lost)
+import sys
+
+input = sys.stdin.readline
 n = int(input())
 lost = list(map(int, input().split()))
 reserve = list(map(int, input().split()))
+
+
+def solution(n, lost, reserve):
+    setLost = set(lost) - set(reserve)
+    setReserve = set(reserve) - set(lost)
+    for i in setReserve:
+        if i - 1 in setLost:
+            setLost.remove(i - 1)
+        elif i + 1 in setLost:
+            setLost.remove(i + 1)
+    return n - len(setLost)
+
+
 print(solution(n, lost, reserve))
