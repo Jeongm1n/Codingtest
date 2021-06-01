776c7a24026f46c0f803bfdbb6c8e9223f181704
Jeongm1n <qll4359@naver.com>
2021-06-01 21:09:18 +0900

    Problem Solving

--- 프로그래머스/Lv.1/키패드누르기.py
+++ 프로그래머스/Lv.1/키패드누르기.py
@@ -1,32 +1,38 @@
-def solution(numbers, hand):
-    answer = ''
-    lastL = 10
-    lastR = 12
-    for n in numbers:
-        if n in [1, 4, 7]:
-            answer += 'L'
-            lastL = n
-        elif n in [3, 6, 9]:
-            answer += 'R'
-            lastR = n
-        else:
-            n = 11 if n == 0 else n
-            absL = abs(n-lastL)
-            absR = abs(n-lastR)
-            if sum(divmod(absL, 3)) > sum(divmod(absR, 3)):
-                answer += 'R'
-                lastR = n
-            elif sum(divmod(absL, 3)) < sum(divmod(absR, 3)):
-                answer += 'L'
-                lastL = n
-            else:
-                if hand == 'right':
-                    answer += 'R'
-                    lastR = n
-                else:
-                    answer += 'L'
-                    lastL = n
-    return answer
+import sys
+
+input = sys.stdin.readline
 numbers = list(map(int, input().split()))
 hand = input()
+
+
+def solution(numbers, hand):
+    lastL, lastR = 10, 12
+    answer = ""
+    for n in numbers:
+        if n in [1, 4, 7]:
+            lastL = n
+            answer += "L"
+        elif n in [3, 6, 9]:
+            lastR = n
+            answer += "R"
+        else:
+            n = 11 if n == 0 else n
+            absL = abs(n - lastL)
+            absR = abs(n - lastR)
+            if sum(divmod(absL, 3)) > sum(divmod(absR, 3)):
+                lastR = n
+                answer += "R"
+            elif sum(divmod(absL, 3)) < sum(divmod(absR, 3)):
+                lastL = n
+                answer += "L"
+            else:
+                if hand == "left":
+                    lastL = n
+                    answer += "L"
+                else:
+                    lastR = n
+                    answer += "R"
+    return answer
+
+
 print(solution(numbers, hand))
