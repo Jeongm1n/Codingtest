657c37d085883d454d9ae2ff7e4f68bc15a344cc
Jeongm1n <qll4359@naver.com>
2021-06-01 16:54:20 +0900

    Problem Solving

--- 프로그래머스/Lv.1/완주하지못한선수.py
+++ 프로그래머스/Lv.1/완주하지못한선수.py
@@ -1,10 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+participant = list(map(str, input().split()))
+completion = list(map(str, input().split()))
+
+
 def solution(participant, completion):
     participant.sort()
     completion.sort()
     for i in range(len(completion)):
-        if participant[i] != completion[i]:
+        if completion[i] != participant[i]:
             return participant[i]
-    return participant[len(completion)]
-participant = list(input().split())
-completion = list(input().split())
+    return participant[-1]
+
+
 print(solution(participant, completion))
