fa89fcd3fffb360343321a10a8f28497b3f208b1
Jeongm1n <qll4359@naver.com>
2021-06-01 21:37:15 +0900

    Problem Solving

--- 프로그래머스/Lv.1/실패율.py
+++ 프로그래머스/Lv.1/실패율.py
@@ -1,15 +1,22 @@
-def solution(N, stages):
-    answer = {} # dictionary를 이용해 {"stage_numer": 실패율}로 처리
-    denominator = len(stages)
-    for stage in range(1, N+1):
-        if denominator != 0:
-            count = stages.count(stage)
-            answer[stage] = count / denominator
-            denominator -= count
-        else:
-            answer[stage] = 0
-    return sorted(answer, key=lambda x : answer[x], reverse=True)
+import sys
+
+input = sys.stdin.readline
 
 n = int(input())
 stages = list(map(int, input().split()))
+
+
+def solution(n, stages):
+    answer = {}
+    length = len(stages)
+    for stage in range(1, n + 1):
+        if length != 0:
+            cnt = stages.count(stage)
+            answer[stage] = cnt / length
+            length -= cnt
+        else:
+            answer[stage] = 0
+    return sorted(answer, key=lambda x: answer[x], reverse=True)
+
+
 print(solution(n, stages))
