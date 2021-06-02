26d636fc742b93eaf60fa42cf83c0b8265d50c1f
Jeongm1n <qll4359@naver.com>
2021-06-02 15:09:47 +0900

    Problem Solving

--- 프로그래머스/Lv.1/1차비밀지도.py
+++ 프로그래머스/Lv.1/1차비밀지도.py
@@ -1,13 +1,20 @@
-def solution(n, arr1, arr2):
-    answer = []
-    for i in range(n):
-        temp1 = bin(arr1[i]|arr2[i])[2:]
-        temp2 = '0'*(n-len(temp1)) + temp1
-        temp2 = temp2.replace('0', ' ')
-        temp2 = temp2.replace('1', '#')
-        answer.append(temp2)
-    return answer
+import sys
+
+input = sys.stdin.readline
 n = int(input())
 arr1 = list(map(int, input().split()))
 arr2 = list(map(int, input().split()))
+
+
+def solution(n, arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        temp1 = bin(arr1[i] | arr2[i])[2:]
+        temp2 = "0" * (n - len(temp1)) + temp1
+        temp2 = temp2.replace("0", " ")
+        temp2 = temp2.replace("1", "#")
+        answer.append(temp2)
+    return answer
+
+
 print(solution(n, arr1, arr2))
