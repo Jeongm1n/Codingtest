a53b1b3b60dfc49778391ff8d432330b0ce99db5
Jeongm1n <qll4359@naver.com>
2021-06-02 13:58:01 +0900

    Problem Solving

--- 프로그래머스/Lv.1/3진법뒤집기.py
+++ 프로그래머스/Lv.1/3진법뒤집기.py
@@ -1,15 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+
 def solution(n):
     temp = []
     answer = 0
     if n == 1:
         return 1
     while n > 1:
-        temp.append(n%3)
+        temp.append(n % 3)
         n //= 3
         if n == 1:
             temp.append(n)
     for i in range(len(temp)):
-        answer += int(temp[i])*(3**(len(temp)-i-1))
+        answer += int(temp[i]) * (3 ** (len(temp) - i - 1))
     return answer
-n = int(input())
+
+
 print(solution(n))
