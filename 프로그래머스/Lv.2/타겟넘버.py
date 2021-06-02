6a3597d438cf418ef10d5e6a71065ff05196249d
Jeongm1n <qll4359@naver.com>
2021-06-02 17:46:24 +0900

    Problem Solving

--- 프로그래머스/Lv.2/타겟넘버.py
+++ 프로그래머스/Lv.2/타겟넘버.py
@@ -1,24 +1,16 @@
+import sys
 from itertools import product
-def solution(numbers, target):
-    '''
-    cnt = 0
-    len_numbers = len(numbers)
-    def operator(idx=0):
-        if idx < len_numbers:
-            numbers[idx] *= 1
-            operator(idx+1)
 
-            numbers[idx] *= -1
-            operator(idx+1)
-        elif sum(numbers) == target:
-            nonlocal cnt
-            cnt += 1
-    operator()
-    return cnt
-    '''
+input = sys.stdin.readline
+
+target = int(input())
+numbers = list(map(int, input().split()))
+
+
+def solution(numbers, target):
     i = [(x, -x) for x in numbers]
     j = list(map(sum, product(*i)))
     return j.count(target)
-numbers = list(map(int, input().split()))
-target = int(input())
+
+
 print(solution(numbers, target))
