7c2867df3043cd857727754f3685033b7bf10739
Jeongm1n <qll4359@naver.com>
2021-06-02 18:34:40 +0900

    Problem Solving

--- 프로그래머스/Lv.2/전화번호목록.py
+++ 프로그래머스/Lv.2/전화번호목록.py
@@ -1,8 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+phone_book = list(map(str, input().split()))
+
+
 def solution(phone_book):
     phone_book.sort()
     for a, b in zip(phone_book, phone_book[1:]):
         if a in b:
-            return False
-    return True
-phone_book = list(map(str, input().split()))
+            print(1)
+
+
 print(solution(phone_book))
