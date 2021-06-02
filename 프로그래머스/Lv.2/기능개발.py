ab18d712b1264a8ee23a5ee4f0840c7f763274ec
Jeongm1n <qll4359@naver.com>
2021-06-02 16:13:54 +0900

    Problem Solving

--- 프로그래머스/Lv.2/기능개발.py
+++ 프로그래머스/Lv.2/기능개발.py
@@ -1,17 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+progresses = list(map(int, input().split()))
+speeds = list(map(int, input().split()))
+
+
 def solution(progresses, speeds):
     answer = []
     while len(progresses) > 0:
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
         if progresses[0] >= 100:
-            cnt = 0 
-            while progresses[0] >= 100:
-                cnt += 1
+            cnt = 0
+            while len(progresses) > 0 and progresses[0] >= 100:
                 progresses.pop(0)
                 speeds.pop(0)
-                if len(progresses) == 0:
-                    break
+                cnt += 1
             answer.append(cnt)
-        progresses = [progresses[i]+speeds[i] for i in range(len(progresses))]
     return answer
-progresses = list(map(int, input().split()))
-speeds = list(map(int, input().split()))
+
+
 print(solution(progresses, speeds))
