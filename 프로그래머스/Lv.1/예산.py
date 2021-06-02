6b06aa0f5e99a926c4cdc4eaca73d01bb8643892
Jeongm1n <qll4359@naver.com>
2021-06-02 13:40:11 +0900

    Problem Solving

--- 프로그래머스/Lv.1/예산.py
+++ 프로그래머스/Lv.1/예산.py
@@ -1,8 +1,20 @@
-def solution(d, budget):
-    d.sort()
-    while budget < sum(d):
-        d.pop()
-    return len(d)
+import sys
+
+input = sys.stdin.readline
+
 d = list(map(int, input().split()))
 budget = int(input())
+
+
+def solution(d, budget):
+    d.sort()
+    temp, answer = 0, 0
+    for i in range(len(d)):
+        temp += d[i]
+        if temp > budget:
+            break
+        answer += 1
+    return answer
+
+
 print(solution(d, budget))
