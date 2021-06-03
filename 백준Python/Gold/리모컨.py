08c0304cc39d27978145b249bdd2b2cfacfbf0e9
Jeongm1n <qll4359@naver.com>
2021-06-03 18:34:34 +0900

    Problem Solving

---
+++ 백준Python/Gold/리모컨.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+enable = {str(i) for i in range(10)}
+n = int(input())
+m = int(input())
+if m != 0:
+    enable -= set(map(str, input().split()))
+
+answer = abs(100 - n)
+
+for num in range(1000001):
+    num = str(num)
+    for j in range(len(num)):
+        if num[j] not in enable:
+            break
+        elif j == len(num) - 1:
+            answer = min(answer, abs(n - int(num)) + len(num))
+
+print(answer)
