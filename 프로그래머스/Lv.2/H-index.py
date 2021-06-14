0cf96939050e2933e246eed3b7b926e7ac02bf66
Jeongm1n <qll4359@naver.com>
2021-06-14 19:15:39 +0900

    Problem Solving

--- 프로그래머스/Lv.2/H-index.py
+++ 프로그래머스/Lv.2/H-index.py
@@ -1,8 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+citations = list(map(int, input().split()))
+
+
 def solution(citations):
     citations.sort()
     for i in range(len(citations)):
-        if citations[i] >= len(citations)-i:
-            return len(citations)-i
+        if len(citations) - i <= citations[i]:
+            return len(citations) - i
     return 0
-citations = list(map(int, input().split()))
+
+
 print(solution(citations))
