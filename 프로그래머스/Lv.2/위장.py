5d2b643f1aefe315c31b6a4999076bc1acebf4ca
Jeongm1n <qll4359@naver.com>
2021-06-14 16:33:41 +0900

    Problem Solving

--- 프로그래머스/Lv.2/위장.py
+++ 프로그래머스/Lv.2/위장.py
@@ -1,13 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+clothes = [list((map(str, input().split()))) for _ in range(int(input()))]
+
+
 def solution(clothes):
-    answer = {}
+    temp = {}
     for i in clothes:
-        if i[1] in answer:
-            answer[i[1]] += 1
+        if i[1] in temp:
+            temp[i[1]] += 1
         else:
-            answer[i[1]] = 1
-    cnt = 1
-    for i in answer.values():
-        cnt *= (i+1)
-    return cnt-1
-clothes = [list(map(str, input().split())) for _ in range(int(input()))]
+            temp[i[1]] = 1
+    answer = 1
+    for i in temp.values():
+        answer *= i + 1
+    return answer - 1
+
+
 print(solution(clothes))
