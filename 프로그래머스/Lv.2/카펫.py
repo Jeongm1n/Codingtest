43a4a2881ca98cad947756f678b1b747875cc175
Jeongm1n <qll4359@naver.com>
2021-06-14 16:05:02 +0900

    Problem Solving

--- 프로그래머스/Lv.2/카펫.py
+++ 프로그래머스/Lv.2/카펫.py
@@ -1,9 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+brown, yellow = map(int, input().split())
+
+
 def solution(brown, yellow):
-    for i in range(1, yellow+1):
-        if yellow % i == 0: # 가로 길이의 경우들 i
-            temp = yellow // i # i에 맞는 세로 길이 temp
-            if 2*i + 2*temp + 4 == brown: # 가로와 세로길이에 맞는 brown 확인
-                return [temp+2, i+2]
-brown = int(input())
-yellow = int(input())
+    for i in range(1, yellow + 1):
+        if yellow % i == 0:
+            temp = yellow // i
+            if i * 2 + temp * 2 + 4 == brown:
+                return [temp + 2, i + 2]
+
+
 print(solution(brown, yellow))
