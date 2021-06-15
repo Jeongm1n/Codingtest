e1412833bedb6671539f219ebcd8d9e9dbe1e0bf
Jeongm1n <qll4359@naver.com>
2021-06-15 19:54:04 +0900

    Problem Solving

--- 프로그래머스/Lv.2/구명보트.py
+++ 프로그래머스/Lv.2/구명보트.py
@@ -1,13 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+limit = int(input())
+people = list(map(int, input().split()))
+
+
 def solution(people, limit):
     people.sort()
     answer = 0
-    i, j = 0, len(people)-1
+    i, j = 0, len(people) - 1
     while i <= j:
         answer += 1
-        if people[i]+people[j] <= limit:
+        if people[i] + people[j] <= limit:
             i += 1
         j -= 1
     return answer
-people = list(map(int, input().split()))
-limit = int(input())
+
+
 print(solution(people, limit))
