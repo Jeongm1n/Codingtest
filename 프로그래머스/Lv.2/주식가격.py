d987b9a43e8169aa8362399219e69a203b040435
Jeongm1n <qll4359@naver.com>
2021-06-15 15:02:58 +0900

    Problem Solving

--- 프로그래머스/Lv.2/주식가격.py
+++ 프로그래머스/Lv.2/주식가격.py
@@ -1,12 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+prices = list(map(int, input().split()))
+
+
 def solution(prices):
     answer = []
     for i in range(len(prices)):
         cnt = 0
-        for j in range(i+1, len(prices)):
+        for j in range(i + 1, len(prices)):
             cnt += 1
             if prices[i] > prices[j]:
                 break
         answer.append(cnt)
     return answer
-prices = list(map(int, input().split()))
+
+
 print(solution(prices))
