5a01fc5fd5190a402cd4d26fb828a75589d6e824
Jeongm1n <qll4359@naver.com>
2021-06-16 19:54:44 +0900

    Problem Solving

---
+++ 프로그래머스/Lv.2/짝지어제거하기.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+s = input().rstrip()
+
+
+def solution(s):
+    stack = []
+    for i in s:
+        if len(stack) == 0:
+            stack.append(i)
+        elif stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
+
+
+print(solution(s))
