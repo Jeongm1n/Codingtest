d7738ed8540bf2bbeb3d6fc7ce0e7898bdc968d0
Jeongm1n <qll4359@naver.com>
2021-06-17 19:43:50 +0900

    Problem Solving

--- 프로그래머스/Lv.3/정수삼각형.py
+++ 프로그래머스/Lv.3/정수삼각형.py
@@ -6,20 +6,15 @@ triangle = [list(map(int, input().split())) for _ in range(int(input()))]
 
 
 def solution(triangle):
-    triangle[1][0], triangle[1][1] = (
-        triangle[0][0] + triangle[1][0],
-        triangle[0][0] + triangle[1][1],
-    )
-    for i in range(2, len(triangle)):
+    for i in range(1, len(triangle)):
         for j in range(len(triangle[i])):
             if j == 0:
                 triangle[i][j] = triangle[i - 1][j] + triangle[i][j]
             elif j == len(triangle[i]) - 1:
                 triangle[i][j] = triangle[i - 1][-1] + triangle[i][j]
             else:
-                triangle[i][j] = max(
-                    triangle[i - 1][j - 1] + triangle[i][j],
-                    triangle[i - 1][j] + triangle[i][j],
+                triangle[i][j] = (
+                    max(triangle[i - 1][j - 1], triangle[i - 1][j]) + triangle[i][j]
                 )
     return max(triangle[-1])
 
