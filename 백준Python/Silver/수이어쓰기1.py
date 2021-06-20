63c610292cb35dc8d2aad67a399e59a6b790394d
Jeongm1n <qll4359@naver.com>
2021-06-20 21:34:56 +0900

    Problem Solving

--- 백준Python/Silver/수이어쓰기1.py
+++ 백준Python/Silver/수이어쓰기1.py
@@ -5,10 +5,10 @@ input = sys.stdin.readline
 n = int(input())
 n = str(n)
 
-temp = 0
+answer = 0
 for i in range(len(n) - 1):
-    temp += 9 * (i + 1) * 10 ** i
+    answer += 9 * (i + 1) * 10 ** i
 
-temp += (int(n) - (10 ** (len(n) - 1)) + 1) * len(n)
+answer += (int(n) - (10 ** (len(n) - 1)) + 1) * len(n)
 
-print(temp)
+print(answer)
