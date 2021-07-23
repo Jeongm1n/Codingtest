7ee1f289a4a396bc8369624527045b18803c81ed
Jeongm1n <qll4359@naver.com>
2021-07-23 17:52:32 +0900

    Problem Solving

---
+++ 프로그래머스/Lv.1/숫자문자열과영단어.py
@@ -0,0 +1,24 @@
+alpha = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+
+def solution(s):
+    start = 0
+    end = 1
+    answer = ""
+    while start < len(s):
+        if s[start].isdigit():
+            answer += s[start]
+            start = end
+            end = end + 1
+            continue
+        if s[start:end] in alpha:
+            answer += str(alpha.index(s[start:end]))
+            start = end
+            end = end + 1
+        else:
+            end += 1
+    return int(answer)
+
+
+s = input()
+print(solution(s))
